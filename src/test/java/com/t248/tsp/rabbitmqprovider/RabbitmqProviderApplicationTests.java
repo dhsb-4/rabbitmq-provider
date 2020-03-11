bb2e058087f4661f316f15b97149d7a7069d6787
dhsb-4 <3042154624@qq.com>
2020-03-11 15:03:01 +0800

    Initial commit

---
+++ src/test/java/com/t248/tsp/rabbitmqprovider/RabbitmqProviderApplicationTests.java
@@ -0,0 +1,14 @@
+package com.t248.tsp.rabbitmqprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("111");
+    }
+
+}
